f56f4bbd24804110ed13303ab4cf930a9ec716b0
TatianaChepovaya <taniacheport@gmail.com>
2021-12-03 17:13:02 +0200

    правка к дз

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,28 @@
-import { NavLink } from 'react-router-dom';
-import s from './Navigation.module.css';
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import authSelectors from "redux/auth/auth-selectors";
+import s from "./Navigation.module.css";
 
 const Navigation = () => {
-    return (
-        <nav className={ s.nav}>
-            <NavLink exact to="/" className={s.link} activeClassName={s.activeLink}>Phonebook</NavLink>
-            <NavLink to="/contacts" className={s.link} activeClassName={s.activeLink}>Contacts</NavLink>
-            </nav>
-    )
-}
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  return (
+    <nav className={s.nav}>
+      <NavLink exact to="/" className={s.link} activeClassName={s.activeLink}>
+        Phonebook
+      </NavLink>
+
+      {isLoggedIn && (
+        <NavLink
+          to="/contacts"
+          exact
+          className={s.link}
+          activeClassName={s.activeLink}
+        >
+          Contacts
+        </NavLink>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
